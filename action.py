0193bcbd979f379a1e0fad11018718b1f2532bc1
juan garces <juan.garces@sonos.com>
2020-05-20 18:19:00 -0700

    missed an f string

--- action.py
+++ action.py
@@ -47,7 +47,7 @@ def invalidate_jamf_token(url, token):
 @logger.catch
 def create_jamf_script(url, token, payload):
     header = { f"Authorization": "Bearer {token}" }
-    script_request = requests.post(url="{url}/uapi/v1/scripts", headers=header, json=payload)
+    script_request = requests.post(url=f"{url}/uapi/v1/scripts", headers=header, json=payload)
     if script_request.status_code == requests.codes.created:
         logger.success("script created")
         return True
@@ -262,7 +262,7 @@ def push_scripts():
             logger.info("it doesn't exist, lets create it")
             #it doesn't exist, we can create it
             with open(script, 'r') as upload_script:
-                payload = {"name": script_name, "info": "", "notes": "created via github action", "priority": "AFTER" , "categoryId": "1", "categoryName":"", "parameter4":"", "parameter5":"", "parameter6":"", "parameter7":"", "parameter8":"", "parameter9":"",  "parameter10":"", "parameter11":"", "osRequirements":"", f"scriptContents":f"{upload_script.read()}"} 
+                payload = {"name": script_name, "info": "", "notes": "created via github action", "priority": "AFTER" , "categoryId": "1", "categoryName":"", "parameter4":"", "parameter5":"", "parameter6":"", "parameter7":"", "parameter8":"", "parameter9":"",  "parameter10":"", "parameter11":"", "osRequirements":"", "scriptContents":f"{upload_script.read()}"} 
                 create_jamf_script(url, token, payload)
         elif len(script_search) == 1:
             jamf_script = script_search.pop()
